c4555ee9d081ee45c9886c9f71eb477c9e6ce26b
thomas-szewczyk <thom.szewczyk@gmail.com>
2025-02-23 11:53:05 +0100

    Initial commit

---
+++ apps/thomas-szewczyk-cv/jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+  displayName: 'thomas-szewczyk-cv',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../coverage/apps/thomas-szewczyk-cv',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
