3efa63d5b9f608e40031ce5a05a6e6d9ed30a0f7
thomas-szewczyk <thom.szewczyk@gmail.com>
2025-03-12 19:38:19 +0100

    Add data-loading logic to LandingComponent
    - Inject `LandingStore` into `LandingComponent` for state management.
    - Implement `OnInit` lifecycle hook in `LandingComponent`.
    - Add `loadData` method to handle calls to `loadSkillGroups`, `loadCvEntries`, and `loadCertificates`.
    - Register `LandingStore` as a provider in `LandingComponent`.

--- libs/landing/feature-landing/src/landing.component.ts
+++ libs/landing/feature-landing/src/landing.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutSectionComponent } from '@thomas-szewczyk-cv/landing/about';
 import { HeroSectionComponent } from '@thomas-szewczyk-cv/landing/hero';
 import { SkillsSectionComponent } from '@thomas-szewczyk-cv/landing/skills';
 import { CvSectionComponent } from '@thomas-szewczyk-cv/landing/cv';
+import { LandingStore } from '@thomas-szewczyk-cv/landing/data-access';
 
 @Component({
   selector: 'lib-landing',
@@ -16,5 +17,18 @@ import { CvSectionComponent } from '@thomas-szewczyk-cv/landing/cv';
   ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
+  providers: [LandingStore],
 })
-export class LandingComponent {}
+export class LandingComponent implements OnInit {
+  store = inject(LandingStore);
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  private loadData() {
+    this.store.loadSkillGroups('');
+    this.store.loadCvEntries('');
+    this.store.loadCertificates('');
+  }
+}
