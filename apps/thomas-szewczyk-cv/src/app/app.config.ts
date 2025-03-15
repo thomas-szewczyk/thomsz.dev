4eaf9c6b44a0a44e47fc5d2dbce397aad689f9b0
thomas-szewczyk <thom.szewczyk@gmail.com>
2025-03-15 15:30:57 +0100

    Set locale to German and update date format in UI
    - Imported and registered German locale data in app configuration.
    - Set `LOCALE_ID` to 'de' for consistent localization.
    - Modified certificate date format to 'MMMM YYYY' in the CV component.

--- apps/thomas-szewczyk-cv/src/app/app.config.ts
+++ apps/thomas-szewczyk-cv/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -6,6 +10,10 @@ import { provideMarkdown } from 'ngx-markdown';
 import { provideHttpClient } from '@angular/common/http';
 import { APP_CONFIG, AppConfig } from '@thomas-szewczyk-cv/core/tokens';
 import { environment } from '../environments/environment';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+
+registerLocaleData(localeDe, 'de');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +22,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(appRoutes),
     provideAnimationsAsync(),
     provideMarkdown(),
+    { provide: LOCALE_ID, useValue: 'de' },
     {
       provide: APP_CONFIG,
       useValue: <AppConfig>{
