9ddcf1e8803733a5ea95464b20cb568a30bbe6ff
thomas-szewczyk <thom.szewczyk@gmail.com>
2025-03-15 15:43:54 +0100

    Enhance skills sorting and add sample database
    - Updated `skills.service.ts` to sort skills by level.
    - Added a sample database `db.json` with CV details, skills, and certificates.
    - Simplified formatting in the `about-section.component.html` for readability.

--- libs/landing/data-access/src/services/skills.service.ts
+++ libs/landing/data-access/src/services/skills.service.ts
@@ -2,6 +2,8 @@ import { inject, Injectable } from '@angular/core';
 import { ApiService } from '@thomas-szewczyk-cv/core/api';
 import { SkillCategory } from '../dtos/skill-category.dto';
 import { APP_CONFIG, AppConfig } from '@thomas-szewczyk-cv/core/tokens';
+import { map } from 'rxjs';
+import { Skill } from '../dtos/skill.dto';
 
 @Injectable({
   providedIn: 'root',
@@ -16,6 +18,17 @@ export class SkillsService {
   }
 
   load() {
-    return this.apiService.get<SkillCategory[]>(this.ENDPOINT);
+    return this.apiService.get<SkillCategory[]>(this.ENDPOINT).pipe(
+      map((skillCategories) =>
+        skillCategories.map((category) => {
+          category.skills = this.sortSkillsByLevel(category.skills);
+          return category;
+        })
+      )
+    );
+  }
+
+  private sortSkillsByLevel(skills: Skill[]) {
+    return skills.sort((a, b) => (b.level ?? 0) - (a.level ?? 0));
   }
 }
