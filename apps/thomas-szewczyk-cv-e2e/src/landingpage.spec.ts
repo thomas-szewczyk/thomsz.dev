57d00515c964b39a6780984486bde5aee3e0c280
thomas-szewczyk <thom.szewczyk@gmail.com>
2025-03-15 12:52:31 +0100

    Refactor E2E tests and optimize CI workflow
    - Removed old example E2E test in `example.spec.ts`.
    - Added E2E tests for landing page layout in `landingpage.spec.ts`.
    - Introduced a Page Object Model (POM) for landing page tests.
    - Simplified and updated GitHub Actions CI workflow:
      - Removed unused permissions and steps.
      - Replaced generic app build with targeted `thomas-szewczyk-cv` build.
      - Added Playwright dependencies and streamlined testing steps.

---
+++ apps/thomas-szewczyk-cv-e2e/src/landingpage.spec.ts
@@ -0,0 +1,53 @@
+import { test, expect } from '@playwright/test';
+import { LandingPage } from './poms/landing-page.pom';
+
+test.describe('Landing Page Layout Tests', () => {
+  let landingPage: LandingPage;
+
+  test.beforeEach(async ({ page }) => {
+    landingPage = new LandingPage(page);
+    await landingPage.goTo();
+  });
+
+  test.describe('Header Tests', () => {
+    test('Header should be visible', async () => {
+      const footer = await landingPage.getHeader();
+      await expect(footer).toBeVisible();
+    });
+  });
+
+  test.describe('Footer Tests', () => {
+    test('Footer should be visible', async () => {
+      const footer = await landingPage.getFooter();
+      await expect(footer).toBeVisible();
+    });
+  });
+
+  test.describe('About Section Tests', () => {
+    test('About section should be visible', async () => {
+      const aboutSection = await landingPage.getAboutSection();
+      await expect(aboutSection).toBeVisible();
+    });
+  });
+
+  test.describe('Skills Section Tests', () => {
+    test('Skills section should be visible', async () => {
+      const skillsSection = await landingPage.getSkillsSection();
+      await expect(skillsSection).toBeVisible();
+    });
+  });
+
+  test.describe('CV Section Tests', () => {
+    test('CV section should be visible', async () => {
+      const cvSection = await landingPage.getCvSection();
+      await expect(cvSection).toBeVisible();
+    });
+  });
+
+  test.describe('Contact Section Tests', () => {
+    test('Contact section should be visible', async () => {
+      const contactSection = await landingPage.getContactSection();
+      await expect(contactSection).toBeVisible();
+    });
+  });
+});
