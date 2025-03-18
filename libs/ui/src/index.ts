58cfab687e3720de183f2705d10f230648de6f7f
thomas-szewczyk <thom.szewczyk@gmail.com>
2025-03-18 16:49:35 +0100

    Add image modal component and integrate with certificate entries
    - Created `ImageModalComponent`.
    - Made `ImageModalComponent` dynamic with Angular Material Dialog and injected data for images.
    - Exported `ImageModalComponent` in the UI library index file.
    - Integrated image modal in `CvCertificateEntryComponent` for viewing larger certificate images.
    - Updated styles to enhance the certificate image appearance and add pointer cursor.

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,2 +1,3 @@
+export * from './components/image-modal/image-modal.component';
 export * from './components/loading-overlay/loading-overlay.component';
 export * from './components/social-logos/social-logos.component';
